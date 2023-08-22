1cab5af0fcda3ad3a8e409696ed66d0ed698403c
Winny <winny.chelangat2@student.moringaschool.com>
2023-08-22 14:17:56 +0300

    Completed lab

--- lib/dog.py
+++ lib/dog.py
@@ -3,5 +3,11 @@
 class Dog:
     # Class body goes here
 
+    
     #Instance method definition
     pass
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")
